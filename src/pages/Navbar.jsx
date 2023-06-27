1c1bbd771025f4dfa7fa726df73c0d0aac1236bd
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2023-06-27 07:42:22 +0600

    Question two navLogo added

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,6 +2,8 @@ import { Navbar, Nav, Container} from "react-bootstrap";
 let style = {
     "width" : '150px',
 }
+let navLogo = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/LeetCode_Logo_black_with_text.svg/2560px-LeetCode_Logo_black_with_text.svg.png";
+
 const Navigation = () => 
 {
     return(
@@ -9,7 +11,7 @@ const Navigation = () =>
         <Navbar bg="light" data-bs-theme="dark" >
           <Container>
             <Navbar.Brand >
-                <img style={style} src="public/logo.png" alt="img is not found" />
+                <img style={style} src={navLogo} alt="img is not found" />
             </Navbar.Brand>
           </Container>
         </Navbar>
